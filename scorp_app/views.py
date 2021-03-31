665bb82b972cca02ea488bad10abbd3af5a3939e
Yusuf <yusuf.topcuoglu531@gmail.com>
2021-03-31 21:59:34 +0300

    optimize imports

--- scorp_app/views.py
+++ scorp_app/views.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404, QueryDict, HttpResponse, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
