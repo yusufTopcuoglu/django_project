3ff0fe282e7579573bce621e24b748cc36fa51f3
Yusuf <yusuf.topcuoglu531@gmail.com>
2021-04-03 19:17:03 +0300

    nested serializer used for postSerializer

--- scorp_app/serializers.py
+++ scorp_app/serializers.py
@@ -11,12 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     # this maybe a HyperlinkedRelatedField to navigate users profile on click
-    owner = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
-    )
+    owner = UserSerializer()
 
     class Meta:
         model = Post
         fields = ['owner', 'created_at', 'image_link']
-        depth = 1
