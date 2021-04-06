f8d3e7855f8d5b349f28d4d82635d32e93d477e5
Yusuf <yusuf.topcuoglu531@gmail.com>
2021-04-06 19:31:30 +0300

    created_at representation as timestamp

--- scorp_app/serializers.py
+++ scorp_app/serializers.py
@@ -13,6 +13,11 @@ class PostSerializer(serializers.ModelSerializer):
     # this maybe a HyperlinkedRelatedField to navigate users profile on click
     owner = UserSerializer()
 
+    def to_representation(self, instance):
+        representation = super(PostSerializer, self).to_representation(instance)
+        representation['created_at'] = instance.created_at.timestamp()
+        return representation
+
     class Meta:
         model = Post
         fields = ['owner', 'created_at', 'image_link']
